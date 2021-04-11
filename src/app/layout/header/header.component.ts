9a219a6609fd697218c3c1317392c45c0e8eb2e0
Ismail Dinar <sm41l.4796@gmail.com>
2021-04-11 15:45:27 +0100

    Add header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { links } from './../../data/links';
+import { AfterViewInit, Component } from '@angular/core';
+import { ILink } from 'src/app/models/link.interface';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
+  public links: ILink[] = links.sort((a,b) => a.position > b.position && 1 || -1);
+  private currentLink: Element;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public ngAfterViewInit(): void {
+    this.currentLink = document.querySelector('.navbar-nav .nav-item a.active');
   }
 
+  public setActiveLink(event: Event): void {
+    const clickedLink = event.target as HTMLLinkElement;
+    this.currentLink.classList.remove('active');
+    clickedLink.classList.toggle('active');
+    this.currentLink = clickedLink;
+  }
 }
