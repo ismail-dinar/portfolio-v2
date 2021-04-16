cf1e437a734ae74b51f528e580a7bab36a3f1d22
Ismail Dinar <sm41l.4796@gmail.com>
2021-04-16 19:23:33 +0100

    Install vanilla tilt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, AfterViewInit, Inject, OnInit } from '@angular/core';
 import { WOW } from 'wowjs/dist/wow.min';
+
+declare var VanillaTilt;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit{
   title = 'portfolio-v2';
+
+  public constructor(@Inject(DOCUMENT) private document: Document){}
+
+  public ngOnInit(): void {
+    VanillaTilt.init(this.document.querySelectorAll(".tiltable-card"), {
+      max: 10,
+      speed: 400,
+      glare: true,
+    });
+
+  }
 
   public ngAfterViewInit(): void {
     new WOW().init();
