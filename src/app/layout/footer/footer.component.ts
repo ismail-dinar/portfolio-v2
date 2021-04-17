fc9cb97b38198f8501c8a1710bd946a76ad60fd5
Ismail Dinar <sm41l.4796@gmail.com>
2021-04-17 18:59:03 +0100

    Add footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  private animationItem: AnimationItem;
+  public socialMedia: string[] = ['facebook', 'linkedin', 'github', 'twitter'];
+  public options: AnimationOptions[] = [];
+  private animationItems: AnimationItem[] = [];
+  public year: number = new Date().getFullYear();
 
-  constructor() { }
+  public constructor(private ngZone: NgZone) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.socialMedia.forEach((item) => {
+      this.options.push({
+        path: `assets/json/${item}.json`,
+        autoplay: false
+      })
+    });
   }
 
+  public animationCreated(animationItem: AnimationItem, index: number): void {
+    this.animationItems[index] = animationItem;
+  }
+
+  public play(index: number): void {
+    this.ngZone.runOutsideAngular(() => this.animationItems[index].play());
+  }
+
+  public stop(index: number): void {
+    this.ngZone.runOutsideAngular(() => this.animationItems[index].stop());
+  }
 }
