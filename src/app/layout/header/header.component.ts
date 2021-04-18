fb5ee4409e8479ea10191a2cfe689c75b39382ac
Ismail Dinar <sm41l.4796@gmail.com>
2021-04-18 16:27:09 +0100

    Mkae navbar fixed

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,7 @@
 import { links } from './../../data/links';
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, HostListener, Inject } from '@angular/core';
 import { ILink } from 'src/app/models/link.interface';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,26 @@ import { ILink } from 'src/app/models/link.interface';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements AfterViewInit {
-  public links: ILink[] = links.sort((a,b) => a.position > b.position && 1 || -1);
+  public links: ILink[] = links.sort(
+    (a, b) => (a.position > b.position && 1) || -1
+  );
   private currentLink: Element;
 
+  public constructor(@Inject(DOCUMENT) private document: Document) {}
+
   public ngAfterViewInit(): void {
-    this.currentLink = document.querySelector('.navbar-nav .nav-item a.active');
+    this.currentLink = this.document.querySelector(
+      '.navbar-nav .nav-item a.active'
+    );
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onscroll(event: Event): void {
+    if (event.target['scrollingElement'].scrollTop) {
+      this.document.querySelector('.navbar').classList.add('glassmorphism');
+    } else {
+      this.document.querySelector('.navbar').classList.remove('glassmorphism');
+    }
   }
 
   public setActiveLink(event: Event): void {
