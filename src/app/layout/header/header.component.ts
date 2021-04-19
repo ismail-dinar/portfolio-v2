909c337413e50a11138495ef66ddde464a1eac18
Ismail Dinar <sm41l.4796@gmail.com>
2021-04-19 20:25:29 +0100

    Handle scroll

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,17 @@
 import { links } from './../../data/links';
-import { AfterViewInit, Component, HostListener, Inject, NgZone, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  Inject,
+  NgZone,
+  OnInit,
+} from '@angular/core';
 import { ILink } from 'src/app/models/link.interface';
 import { DOCUMENT } from '@angular/common';
 import { AnimationOptions } from 'ngx-lottie';
 import { AnimationItem } from 'lottie-web';
-
+import { find } from 'lodash';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -15,21 +22,25 @@ export class HeaderComponent implements AfterViewInit, OnInit {
     (a, b) => (a.position > b.position && 1) || -1
   );
   private currentLink: Element;
-  public options: AnimationOptions;;
+  public options: AnimationOptions;
   private animationItem: AnimationItem;
   private animated: boolean = false;
 
-  public constructor(@Inject(DOCUMENT) private document: Document, private ngZone: NgZone) {}
+  public constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private ngZone: NgZone
+  ) {}
 
   public ngOnInit(): void {
     this.options = {
       path: 'assets/json/menu.json',
       autoplay: false,
       loop: false,
-    }
+    };
   }
 
   public ngAfterViewInit(): void {
+    this.setLinksObserver();
     this.currentLink = this.document.querySelector(
       '.navbar-nav .nav-item a.active'
     );
@@ -45,10 +56,20 @@ export class HeaderComponent implements AfterViewInit, OnInit {
   }
 
   public setActiveLink(event: Event): void {
+    event.preventDefault();
     const clickedLink = event.target as HTMLLinkElement;
     this.currentLink.classList.remove('active');
     clickedLink.classList.toggle('active');
     this.currentLink = clickedLink;
+    this.document.querySelector(this.currentLink.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    })
+  }
+
+  public toggleActiveLink(link: string): void {
+    console.log(link);
+    this.document.querySelector('.link.active').classList.remove('active');
+    this.document.querySelector('#link-to-' + link).classList.toggle('active');
   }
 
   public changeNavbarBg(): void {
@@ -61,12 +82,31 @@ export class HeaderComponent implements AfterViewInit, OnInit {
   }
 
   public toggleAnimation(): void {
-    if(!this.animated) {
-      this.ngZone.runOutsideAngular(() => {this.animationItem.play();});
-    }else {
-      this.ngZone.runOutsideAngular(() => {this.animationItem.stop();});
+    if (!this.animated) {
+      this.ngZone.runOutsideAngular(() => {
+        this.animationItem.play();
+      });
+    } else {
+      this.ngZone.runOutsideAngular(() => {
+        this.animationItem.stop();
+      });
     }
     this.animated = !this.animated;
   }
 
+  private setLinksObserver(): void {
+    const links = this.document.querySelectorAll('a.link');
+    const io = new IntersectionObserver(
+      (entries: IntersectionObserverEntry[]) => {
+        const visibleSection = find(entries, 'isIntersecting')?.target;
+        if (visibleSection) {
+          this.toggleActiveLink(visibleSection.id);
+        }
+      },
+      { threshold: 1.0 }
+    );
+    links.forEach((link) =>
+      io.observe(this.document.querySelector(link.getAttribute('href')))
+    );
+  }
 }
