978a6dc4172d84a3bc821fb514f682ab1f1432c2
Ismail Dinar <sm41l.4796@gmail.com>
2021-04-20 20:55:17 +0100

    About me section styling

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { person } from 'src/app/data/person';
+import { IPerson } from 'src/app/models/person.interface';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
-  styleUrls: ['./about-me.component.scss']
+  styleUrls: ['./about-me.component.scss'],
 })
 export class AboutMeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public readonly person: IPerson = person;
+  public fullName: string;
+  public constructor() {
+    this.fullName = this.person.firstName + ' ' + this.person.lastName;
   }
 
+  public ngOnInit(): void {
+  }
 }
