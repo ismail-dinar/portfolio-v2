42069a281a99013679eae84cb0875253bdb47141
Ismail Dinar <sm41l.4796@gmail.com>
2021-04-22 20:38:19 +0100

    Populate the resume section

--- src/app/components/resume/resume.component.ts
+++ src/app/components/resume/resume.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
-  styleUrls: ['./resume.component.scss']
+  styleUrls: ['./resume.component.scss'],
 })
 export class ResumeComponent implements OnInit {
+  public options: AnimationOptions;
+  private animationItem: AnimationItem;
 
-  constructor() { }
+  public constructor(private ngZone: NgZone) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.options = {
+      path: 'assets/json/download.json',
+      autoplay: false,
+      loop: false,
+    };
   }
 
+  public animationCreated(animationItem: AnimationItem){
+    this.animationItem = animationItem;
+  }
+
+  public play(): void {
+    this.ngZone.runOutsideAngular(() => {this.animationItem.play()})
+  }
+
+  public onComplete(): void {
+    this.ngZone.runOutsideAngular(() => {this.animationItem.resetSegments(true)})
+  }
 }
